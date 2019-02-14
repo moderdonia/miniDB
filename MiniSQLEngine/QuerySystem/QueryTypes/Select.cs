43e53497ddde44821beeb9c54b44f589656b827d
Moderdonia <ugonzalez028@ikasle.ehu.eus>
2019-02-14 12:04:19 +0100

    MiniSQLEngine v.1

---
+++ MiniSQLEngine/QuerySystem/QueryTypes/Select.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniSQLEngine.QuerySystem.QueryTypes
+{
+    class Select : SQLtype
+    {
+        private string table;
+        private string database;
+        private string[] attb;         //columns
+        private string[] conds;    //where
+
+        bool Execute()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Select(string table, string database, string[] attb, string[] conds)
+        {
+            this.table = table;
+            this.database = database;
+            this.attb = attb;
+            this.conds = conds;
+        }
+    }
+}
---
+++ MiniSQLEngine/QuerySystem/SQLtype.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniSQLEngine
+{
+    public abstract class SQLtype    
+    {
+
+        bool Execute()
+        {
+            return false;
+        }
+
+    }
+}
