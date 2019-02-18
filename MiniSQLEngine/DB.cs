d7d22ebe04df50e732d4cb84bbabe4a7badc251c
videojuegos.eui-vi <videojuegos.eui-vi@hotmail.com>
2019-02-18 13:23:33 +0100

    BD

--- MiniSQLEngine/DB.cs
+++ MiniSQLEngine/DB.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,10 +9,14 @@ namespace MiniSQLEngine
 {
     class DB
     {
+        Hashtable ht;
+
         string name;
 
         public DB(string name)
         {
+            ht = new Hashtable();
+
             this.name = name;
         }
 
