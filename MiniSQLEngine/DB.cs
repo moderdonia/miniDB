0a75da81316fee92ab3d84350dbcf0822ce19a69
videojuegos.eui-vi <videojuegos.eui-vi@hotmail.com>
2019-02-18 12:37:20 +0100

    BD y Parser

---
+++ MiniSQLEngine/DB.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniSQLEngine
+{
+    class DB
+    {
+        string name;
+
+        public DB(string name)
+        {
+            this.name = name;
+        }
+
+        public string runQuery(string query)
+        {
+            return null;
+        }
+    }
+}
