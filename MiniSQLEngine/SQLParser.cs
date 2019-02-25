0f59f86d26e1e78163c128f15785889b714d030b
videojuegos.eui-vi <videojuegos.eui-vi@hotmail.com>
2019-02-25 13:23:51 +0100

    Hola

--- MiniSQLEngine/SQLParser.cs
+++ MiniSQLEngine/SQLParser.cs
@@ -11,7 +11,7 @@ namespace MiniSQLEngine
     public class SQLParser
     {
 
-        public SQLtype parser(string query)
+        public SQLtype Parser(string query)
         {
             //Con *:
             string select1 = @"SELECT\s+(\*)\s+FROM\s+(\w+);";
@@ -26,6 +26,7 @@ namespace MiniSQLEngine
             string insert = @"(INSERT)\s+(INTO)\s+(\w+)(\s+|\s+\((\w+)\)\s+|\s+\((\w+)(\,\s+(\w+))+\))\s+(VALUES)(\s+\((\w+)\)|\s+\((\w+)(\,\s+(\w+))+\));";
             string delete = @"(DELETE)\s + (FROM)\s + (\w +)\s + (WHERE)\s +\w +\s * (=|<|>)\s *\w +;";
             string update = @"(UPDATE)\s+(\w+)\s+(SET)\s+(\w+)\s*\=\s*(\w+)(\s+|\,\s+(\w+)\s*\=\s*(\w+)\s+)(WHERE)\s+(\w+)\s*(=|<|>)\s*(\w+);";
+
             string dropTable = @"(DROP)\s+(TABLE)\s+(\w+);";
             string dropDB = @"(DROP)\s+(DATABASE)\s+(\w+);";
             string createDB = @"(CREATE)\s+(DATABASE)\s+(\w+);";
@@ -35,8 +36,15 @@ namespace MiniSQLEngine
             string[] camp1 = null;
             string camp2;
             string[] camp3 = null;
+            string campo1;
+
             //SELECT
             Match matchS1 = Regex.Match(query, select1);
+            Match matchS2 = Regex.Match(query, select2);
+            Match matchS3 = Regex.Match(query, select3);
+            Match matchS4 = Regex.Match(query, select4);
+            Match matchS5 = Regex.Match(query, select5);
+            Match matchS6 = Regex.Match(query, select6);
 
             if (Regex.Match(query, select1).Success)
             {
@@ -49,40 +57,74 @@ namespace MiniSQLEngine
             }
             else if (Regex.Match(query, select2).Success)
             {
+                camp1[0] = matchS2.Groups[0].Value;
+                camp2 = matchS2.Groups[1].Value;
+                camp3[0] = matchS2.Groups[2].Value;
+                camp3[1] = matchS2.Groups[2].Value;
+                camp3[2] = matchS2.Groups[2].Value;
+
+                SQLtype sentencia = new Select(camp2, camp1, camp3);
+
+                return sentencia;
             }
+            else if (Regex.Match(query, select3).Success)
+            {
+                camp1[0] = matchS3.Groups[0].Value;
+                camp2 = matchS3.Groups[1].Value;
+                
+                SQLtype sentencia = new Select(camp2, camp1, camp3);
 
-            Match matchS2 = ;
-            Match matchS3 = Regex.Match(query, select3);
-            Match matchS4 = Regex.Match(query, select4);
-            Match matchS5 = Regex.Match(query, select5);
-            Match matchS6 = Regex.Match(query, select6);
+                return sentencia;
+            }
+            else if (Regex.Match(query, select4).Success)
+            {
+                camp1[0] = matchS4.Groups[0].Value;
+                camp2 = matchS4.Groups[1].Value;
+                camp3[0] = matchS4.Groups[2].Value;
+                camp3[1] = matchS4.Groups[3].Value;
+                camp3[2] = matchS4.Groups[4].Value;
 
+                SQLtype sentencia = new Select(camp2, camp1, camp3);
 
-            
-            else if (matchS2.Success)
+                return sentencia;
+            }
+            else if (Regex.Match(query, select5).Success)
+            {
+                camp1[0] = matchS5.Groups[0].Value;
+                camp1[1] = matchS5.Groups[2].Value;
+                camp2 = matchS5.Groups[3].Value;
+                
+                SQLtype sentencia = new Select(camp2, camp1, camp3);
+
+                return sentencia;
+            }
+            else if (Regex.Match(query, select6).Success)
             {
 
+                camp1[0] = matchS6.Groups[0].Value;
+                camp1[1] = matchS6.Groups[2].Value;
+                camp2 = matchS6.Groups[3].Value;
+                camp3[0] = matchS6.Groups[4].Value;
+                camp3[1] = matchS6.Groups[5].Value;
+                camp3[2] = matchS6.Groups[6].Value;
+
+                SQLtype sentencia = new Select(camp2, camp1, camp3);
+
+                return sentencia;
             }
-            else if (matchS3.Success)
+
+
+            Match matchDT = Regex.Match(query, dropTable);
+
+            if (matchDT.Success)
             {
+                campo1 = matchDT.Groups[0].Value;
 
-            }
-            else if (matchS4.Success)
-            {
+                SQLtype sentencia = new DropTable(campo1);
 
-            }
-            else if (matchS5.Success)
-            {
-
-            }
-            else if (matchS6.Success)
-            {
-
+                return sentencia;
             }
 
-
-
-            Match match2 = Regex.Match(query, insert);
             Match match3 = Regex.Match(query, delete);
             Match match4 = Regex.Match(query, update);
             Match match5 = Regex.Match(query, dropTable);
