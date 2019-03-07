c86052d84202f64e694dc2439b2bdfe7407d708b
videojuegos.eui-vi <videojuegos.eui-vi@hotmail.com>
2019-03-07 11:47:44 +0100

    Column object

---
+++ MiniSQLEngine/QuerySystem/Columna.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniSQLEngine.QuerySystem
+{
+    class Columna
+    {
+        public string nombre { get; set; }
+        public string tipo { get; set; }
+
+        public Columna(string nombre, string tipo)
+        {
+            this.nombre = nombre;
+            this.tipo = tipo;
+        }
+    }
+}
