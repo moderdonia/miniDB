5ae5e33965b53f98620dceb6bf43c60474ed41d9
videojuegos.eui-vi <videojuegos.eui-vi@hotmail.com>
2019-03-07 12:53:16 +0100

    Parse Column

---
+++ MiniSQLEngine/QuerySystem/Column.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniSQLEngine.QuerySystem
+{
+    public class Column
+    {
+        public string name { get; set; }
+        public string type { get; set; }
+
+        public Column(string name, string type)
+        {
+            this.name = name;
+            this.type = type;
+        }
+    }
+}
