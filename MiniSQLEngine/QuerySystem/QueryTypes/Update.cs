734f4d1d636e2e38d5724d94d717cc89dcd97d34
videojuegos.eui-vi <videojuegos.eui-vi@hotmail.com>
2019-03-14 12:58:26 +0100

    new

--- MiniSQLEngine/QuerySystem/QueryTypes/Update.cs
+++ MiniSQLEngine/QuerySystem/QueryTypes/Update.cs
@@ -15,7 +15,8 @@ namespace MiniSQLEngine.QuerySystem.QueryTypes
 
         public override string Execute(DB database)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            return null;
         }
 
         public Update(string table, string[] attb, string[] conds)
